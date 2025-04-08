1b4ec9f43328ac2ebcbbcb65f4ce02518bf2ec88
AlanGuo <AlanGuo>
2025-04-08 15:12:26 +0800

    funding frontend

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -829,7 +829,7 @@ export default function UserPage() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="h-6 w-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  <CardTitle className="text-lg">市值</CardTitle>
+                  <CardTitle className="text-lg">总资产市值</CardTitle>
                 </div>
                 <div className="font-semibold text-lg">
                   ${totalMarketValue.toLocaleString()}
@@ -880,7 +880,7 @@ export default function UserPage() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="h-6 w-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                   </svg>
-                  <CardTitle className="text-lg">收益</CardTitle>
+                  <CardTitle className="text-lg">总收益</CardTitle>
                 </div>
                 <div className="font-semibold text-lg">
                   {(() => {
